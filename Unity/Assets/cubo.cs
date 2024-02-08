7ed555b8878623f8a052d5b4125091b16e8b1e2e
4r1ll0 <ivan.diaz0306@gmail.com>
2024-02-08 16:09:59 +0000

    08/02/2024

--- Unity/Assets/cubo.cs
+++ Unity/Assets/cubo.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class cubo : MonoBehaviour
 {
-    //Vector2 direccionArriba = new Vector2(0, 10);
+    public Vector3 posicionFinal = new Vector3 (5,0,0); 
     public float speed = 5;
+
     void Update()
     {
+        /*if (transform.position.x <= posicionFinal.x )
+        {
+            transform.Translate(movimiento * Time.deltaTime * speed);
 
-        //transform.Translate(direccionArriba * speed * Time.deltaTime);
-       transform.position = new Vector2(2, 0);
+        }*/
+
+        /*Comparar la position actual del cubo con la posicion final a la que quieres llegar
+         * Si ha llegado, cambiar de direccion
+         * Si no ha llegado, moverse hacia esa dieccion
+         * 
+         * 
 
     }
 }
