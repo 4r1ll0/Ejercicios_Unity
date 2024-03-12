410a214e12aa0a0d80cee33886154c306e0c8b88
4r1ll0 <ivan.diaz0306@gmail.com>
2024-03-12 16:46:22 +0000

    12/03/2024

---
+++ Unity/Assets/moverobjeto.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverobjeto : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+        //transform.localScale = new Vector2(5, 10);
+        transform.localRotation = Quaternion.Euler (-70, 50, 90);
+
+    }
+}
+
+
